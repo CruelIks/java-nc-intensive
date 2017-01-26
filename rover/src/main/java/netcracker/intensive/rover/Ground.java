660d5eecd1c5b1a58cbbe7c891f3d1644c9ca7dc
CruelIks <iks.gog@gmail.com>
2017-01-26 12:22:14 +0300

    Реализован класс  Ground

--- rover/src/main/java/netcracker/intensive/rover/Ground.java
+++ rover/src/main/java/netcracker/intensive/rover/Ground.java
@@ -1,6 +1,41 @@
 package netcracker.intensive.rover;
 
+/**
+ * Класс описывает игровое поле. Состоит из клеток (GroundCell)
+ */
 public class Ground {
 
+    private GroundCell[][] landscape;
+    //высота поля
+    private int height;
+    //ширина поля
+    private int width;
 
+    public Ground(int height, int width) {
+        this.height = height;
+        this.width = width;
+        landscape = new GroundCell[height][width];
+    }
+
+    public void initialize(GroundCell... cells) {
+
+        if (cells.length == 0) {
+            return;
+        }
+        if (cells.length > height * width) {
+            throw new RuntimeException("Too much cells");
+        }
+
+        int x = 0, y = 0;
+
+        for (GroundCell cell : cells) {
+            landscape[y][x] = cell;
+
+            x++;
+            if (x == width) {
+                x = 0;
+                y++;
+            }
+        }
+    }
 }
