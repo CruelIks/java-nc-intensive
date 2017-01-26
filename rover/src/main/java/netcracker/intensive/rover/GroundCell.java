58c784c26de5c22baa83c7d67f531e1250ed970c
CruelIks <iks.gog@gmail.com>
2017-01-26 10:54:44 +0300

    Реализован класс GroundCell

--- rover/src/main/java/netcracker/intensive/rover/GroundCell.java
+++ rover/src/main/java/netcracker/intensive/rover/GroundCell.java
@@ -1,5 +1,39 @@
 package netcracker.intensive.rover;
 
+import netcracker.intensive.rover.constants.CellState;
+
+/**
+ * Класс описывает ячейку поля
+ *
+ * */
 public class GroundCell {
 
+    private Point point;
+    private CellState state;
+
+    public GroundCell(Point point) {
+        this.point = point;
+        this.state = CellState.FREE;
+    }
+
+    public GroundCell(Point point, CellState state) {
+        this.point = point;
+        this.state = state;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    public CellState getState() {
+        return state;
+    }
+
+    public void setState(CellState state) {
+        this.state = state;
+    }
 }
